f7daeca9707d1e6333facee231324d88dcb4b73f
karangorania <karangoraniya@gmail.com>
2022-08-17 17:59:34 +0530

    edit: added routes for sendCustomToken file 🚀

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const port = process.env.PORT || 3000;
 
 const userRouter = require('./routes/sendToken');
 const walletRouter = require('./routes/createWallet');
+const sendTokenRouter = require('./routes/sendCustomToken');
 
 // Built in JSON Middleware
 app.use(express.json());
@@ -13,6 +14,8 @@ app.use(express.json());
 app.post('/user', userRouter);
 
 app.post('/wallet', walletRouter);
+
+app.post('/sendToken', sendTokenRouter);
 
 app.listen(port, () => {
   console.log(`Connected to ${port}`);
